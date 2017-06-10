b0f6c8166b33c3d2013d53897848877770609d19
2ayan <bandyopadhyay.ayan@gmail.com>
2017-06-10 09:13:43 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+ <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+  <head>
+    <META http-equiv="Content-Style-Type" content="text/css">
+  <link rel="stylesheet" href="style.css" type="text/css">
+  <title>Fountain Pen Review</title>
+  </head>
+<body>
+  <div class="body">
+    <div class="bodyrow">
+    <div class="right">
+    </div>
+    <div style="width=90%">
+      <img src="Ayan.jpg">
+      </div>
+    <div class="right">
+    </div>
+  </div>
+  </div>
+  
+  <div class="body">
+    <div class="bodyrow">
+      <div class="right">
+    
+    </div>
+<!-- Main body -->
+    <?php
+    include("body.html");
+    ?>
+<!-- -->    
+    <div class="right">
+    </div>
+    
+    </div> <!--end of bodyrow div -->
+  </div><!--end of body div -->
+</body>
+</html> 
